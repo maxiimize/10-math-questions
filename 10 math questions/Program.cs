c95d7df0220c3af0ae6cebaa360e08799b8d7224
maxiimize <max.b@hotmail.se>
2024-09-10 11:36:09 +0200

    Till question 5

--- 10 math questions/Program.cs
+++ 10 math questions/Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Question 1: Addition of two numbers
+            //Console.WriteLine("Ange första talet som du vill addera: ");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Ange andra talet som du vill addera: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //int addition = num + num2;
+            //Console.WriteLine($"Resultatet är: {addition}");
+
+
+            //Question 2: Multiplication table
+            //Console.WriteLine("Ange det tal som du vill ha multiplikationstabellen för: ");
+            //int num = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i * num);
+            //}        
+
+
+            //Question 3: Calculate the square of a number
+            //Console.WriteLine("Ange ett tal som du vill räkna ut kvadratroten på: ");
+            //float num = float.Parse(Console.ReadLine());
+            //float numSqrt = (float)Math.Sqrt(num);
+            //float numSqrtTwoDec = (float)Math.Round(numSqrt, 2);
+
+            //Console.WriteLine($"Kvadratroten blir: {numSqrtTwoDec}");
+
+
+            //Question 4: Calculate average value
+            //Console.WriteLine("Ange första talet: ");
+            //float num1 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Ange andra talet: ");
+            //float num2 = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Ange tredje talet: ");
+            //float num3 = float.Parse(Console.ReadLine());
+
+            //float avgValue = (num1 + num2 + num3) / 3;
+            //float avgValueTwoDec = (float)Math.Round(avgValue, 1);
+
+            //Console.WriteLine($"Medelvärdet på talen du uppgav är: {avgValueTwoDec}");
+
+
+            //Question 5
         }
     }
 }
